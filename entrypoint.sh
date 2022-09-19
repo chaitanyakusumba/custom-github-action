844dea461173cb03ff67fa9fd62e1cc5faa084c6
chaitanyakusumba <79130551+chaitanyakusumba@users.noreply.github.com>
2022-09-19 18:35:34 +0530

    Report generate format changed to csv

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,8 +22,8 @@ case "$1" in
         source MyEnv/bin/activate
         pip3 install -r requirements.txt
         pip3 install bs4 lxml
-        license_finder report --python-version=3 --format html > license.html
-        python3 parser.py > license.csv
+        license_finder report --python-version=3 --format csv > license.csv
+        # python3 parser.py > license.csv
         ;;
     node)
         cp -rf . .
@@ -33,7 +33,7 @@ case "$1" in
         gem install license_finder
         npm install -g npm@7.5.6
         npm install 
-        license_finder report --format html > license.html
-        python3 parser.py > license.csv
+        license_finder report --format csv > license.csv
+        # python3 parser.py > license.csv
         ;;
 esac
